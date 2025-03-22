b84b85f32948c6153da5398382c82a9b13beed41
Mateus Aureliano <mateusudo32@gmail.com>
2025-03-22 12:13:35 -0300

    realizei o código do exercicioFuncionarios.php

--- AtividadeOOP/exercicioFuncionarios.php
+++ AtividadeOOP/exercicioFuncionarios.php
@@ -1,7 +1,57 @@
 <?php
+
+class Funcionarios {
+    public $nome;
+    protected $salario;
+    private $senha;
+
+
+    function __construct($nome, $salario, $senha) {
+        $this->nome = $nome;
+        $this->salario = $salario;
+        $this->senha = $senha;
+    }
+
+    // Exibir nome e salário, eu errei na lógica pois defini os parâmetros e não deixei para exibir as informações
+    public function exibirInformacoes() {
+        return "Nome: {$this->nome}, Salário: R$ {$this->salario}\n";
+    }
+
     
+    public function getterSalario() {
+        return $this->salario;
+    }
+
+    
+    public function setterSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    
+    public function setSalario($novoSalario) {
+        $this->salario = $novoSalario;
+    }
 
 
+    public function aumentarSalario($percentual) {
+        $this->salario += ($this->salario * $percentual / 100);
+    }
+}
 
+
+$funcionario = new Funcionarios("João", 1200, "123456");
+
+// Atualizar salário
+$funcionario->setSalario(1500);
+
+// Alterar a senha
+$funcionario->setterSenha("nova_senha");
+
+// Aumentar salário em 20%
+$funcionario->aumentarSalario(20);
+
+echo $funcionario->exibirInformacoes(); // Nome e salário atualizados
+
+echo "O salário do funcionário é de: R$ " . $funcionario->getterSalario() . "\n";
 
 ?>
