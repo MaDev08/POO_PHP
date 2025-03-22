878c0298b8747a007366036ac1e69fb4fcdcdc29
Mateus Aureliano <mateusudo32@gmail.com>
2025-03-22 20:22:15 -0300

    finalizado o código do arquivo exercicioCadastro.php

--- AtividadeOOP/exercicioCadastro.php
+++ AtividadeOOP/exercicioCadastro.php
@@ -1,31 +1,41 @@
 <?php
-    
-    class Produtos{
-       public string $nome;
-       public float $preco;
-       public int $quantidade;
 
-       public function __construct($nome, $preco, $quantidade){
-            $this->nome = $nome;
-            $this->preco = $preco;
-            $this->quantidade = $quantidade;
-        }
+class Produtos{
+    public string $nome;
+    public float $preco;
+    public int $quantidade;
 
-        public function exibirInformacoes(){
-            return "Nome: {$this->nome}, preço: {$this->preco}, quantidade: {$this->quantidade}";
-        }
-
-        public function atualizarQuantidade($novaQuantidade){
-            $this-> quantidade = $novaQuantidade;
-        }
-
-        public function calcularValorTotal(){
-            return $this->preco * $this->quantidade;
-        }
+    public function __construct($nome, $preco, $quantidade){
+        $this->nome = $nome;
+        $this->preco = $preco;
+        $this->quantidade = $quantidade;
     }
 
-//É preciso passar o valor dos parâmetros do consstrutor no objeto SEMPRE!!!
-$produto = new Produtos("óleo de  peroba", 12.99, 50);
-    
+    public function exibirInformacoes(){
+        return "Nome: {$this->nome}, preço: {$this->preco}, quantidade: {$this->quantidade}";
+    }
+
+    public function atualizarQuantidade($novaQuantidade){
+        $this->quantidade = $novaQuantidade;
+    }
+
+    public function calcularValorTotal(){
+        return $this->preco * $this->quantidade;
+    }
+}
+
+
+$produto1 = new Produtos("Óleo de Peroba", 12.99, 50);
+
+echo $produto1->exibirInformacoes(); 
+
+$produto1->atualizarQuantidade(60);
+$produto2->atualizarQuantidade(25);
+
+//PHP_EOL = \n do HTML
+
+echo $produto1->exibirInformacoes() . PHP_EOL;
+echo "Valor total do estoque de {$produto1->nome}: R$ " . $produto1->calcularValorTotal() . PHP_EOL;
+echo "Valor total do estoque de {$produto2->nome}: R$ " . $produto2->calcularValorTotal() . PHP_EOL;
 
 ?>
