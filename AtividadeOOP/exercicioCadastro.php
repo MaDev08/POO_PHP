e4c1ae7f02e3c0d20db032f89151d4dcd58d4337
Mateus Aureliano <mateusudo32@gmail.com>
2025-03-22 12:38:20 -0300

    Adicionado o código no arquivo exercicioCadastro.php , porém está incompleto

--- AtividadeOOP/exercicioCadastro.php
+++ AtividadeOOP/exercicioCadastro.php
@@ -1,7 +1,31 @@
 <?php
     
+    class Produtos{
+       public string $nome;
+       public float $preco;
+       public int $quantidade;
 
+       public function __construct($nome, $preco, $quantidade){
+            $this->nome = $nome;
+            $this->preco = $preco;
+            $this->quantidade = $quantidade;
+        }
 
+        public function exibirInformacoes(){
+            return "Nome: {$this->nome}, preço: {$this->preco}, quantidade: {$this->quantidade}";
+        }
 
+        public function atualizarQuantidade($novaQuantidade){
+            $this-> quantidade = $novaQuantidade;
+        }
+
+        public function calcularValorTotal(){
+            return $this->preco * $this->quantidade;
+        }
+    }
+
+//É preciso passar o valor dos parâmetros do consstrutor no objeto SEMPRE!!!
+$produto = new Produtos("óleo de  peroba", 12.99, 50);
+    
 
 ?>
